eeef88fe33a78816e1ee384cfad000151a8ff6ca
neves-gabriel <gabrielrn.mobres@gmail.com>
2021-08-30 14:46:09 -0300

    add: quantity counter

--- src/components/Dish.js
+++ src/components/Dish.js
@@ -1,18 +1,37 @@
 import { useState } from 'react'
+import Quantity from './Quantity';
 
 export default function Dish(props) {
     const { imgSrc, imgAlt, dishTitle, dishDescription, dishPrice } = props.dishData;
     const [dishClass, setDishClass] = useState("opcao");
+    const [checkClass, setCheckClass] = useState("check");
+    const [dishAmount, setDishAmount] = useState(1);
+
+    function removeQty() {
+        if (dishAmount <= 1) {
+            setDishClass("opcao")
+            setCheckClass("check")
+            console.log(dishAmount)
+            setDishAmount(0)
+        } else {
+            setDishAmount(dishAmount - 1);
+        }
+    }
+
+    function addQty() {
+        setDishAmount(dishAmount + 1);
+    }
 
     return (   
-        <div class= {dishClass} onClick={() => setDishClass("opcao_selecionada")}>
+        <div class= {dishClass} onClick={() => {setDishClass("opcao_selecionada");
+            setCheckClass("check_selecionado");}}>
             <div class="container-img">
                 <img src={imgSrc} alt={imgAlt}/>
             </div>
             <p class="item">{dishTitle}</p>
             <p class="descricao">{dishDescription}</p>
             <p class="preco">{dishPrice}</p>
-            <p class="check"></p>
+            <Quantity checkClass={checkClass} dishAmount={dishAmount} removeQty={removeQty} addQty={addQty} />
         </div>
     );
 }
