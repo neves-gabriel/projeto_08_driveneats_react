b4b4a6d759f9f0e9bbb2b57291d58e7e58daa796
Matheus Galdino <matheus@respondeai.com.br>
2021-08-31 12:55:49 -0300

    preparando o review

--- src/components/Header.js
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <div class="barra-topo">
-            <p class="logotipo">DrivenEats</p>
-            <p class="sublogo">Sua comida em 6 minutos</p>
-    </div>
-    );
-}
